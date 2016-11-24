76f9ca4319acc8f6b2636c5a4edea7e016111bca
s-hinse <webdev@svenhinse.de>
2016-11-24 20:31:30 +0100

    added access-control headers

--- REST-filepress.php
+++ REST-filepress.php
@@ -52,6 +52,7 @@ class Plugin {
 		header( 'Access-Control-Allow-Origin: *' );
 		header( 'Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, DELETE' );
 		header( 'Access-Control-Allow-Credentials: true' );
+		header( 'Access-Control-Allow-Headers: Content-Type, Authorization' );
 
 		return $value;
 	}
